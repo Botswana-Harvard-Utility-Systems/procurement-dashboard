3de2ddf4397bfc3272f1faa122360de5df1bcaa1
tumiemosweu <imosweuk@gmail.com>
2021-06-01 08:47:19 +0200

    update set file

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='procurement-dashboard',
-    version='0.0',
+    version='0.1.0',
     author=u'Software Engineering & Data Management',
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
